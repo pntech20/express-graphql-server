4c5f86e44577d2b54b41c00ec291933f8e13b77f
pntech20 <pntech20@gmail.com>
2020-08-16 13:04:30 +0700

    RootQuery & Fetching Data

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const expressGQL = require('express-graphql')
-const schema = require('./schema')
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema.js');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 4000
 
-app.use('/graphql', expressGQL({
-  schema,
-  graphiql: true
-}))
+// enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
+app.use(cors());
 
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
